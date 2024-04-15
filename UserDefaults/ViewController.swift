3e03b60ea929393868b8605fd15b05bc09920e82
Sandy Gomez <sgomez1029@gmail.com>
2024-04-15 12:21:52 -0700

    Saving Data in UserDefaults in Storyboards

--- UserDefaults/ViewController.swift
+++ UserDefaults/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mySlider: UISlider!
+    @IBOutlet var mySwitch: UISwitch!
+    @IBOutlet var myTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func saveData(_ sender: UIButton) {
+        UserDefaults.standard.set(myTextField.text, forKey: "Text")
+        UserDefaults.standard.set(mySwitch.isOn, forKey: "Switch")
+        UserDefaults.standard.set(mySlider.value, forKey: "Slider")
+    }
+    
+    @IBAction func clearData(_ sender: UIButton) {
+        mySwitch.isOn = true
+        mySlider.value = 0.5
+        myTextField.text = ""
+        
+    }
+    
+    @IBAction func loadData(_ sender: UIButton) {
+        mySwitch.isOn = UserDefaults.standard.bool(forKey: "Switch")
+        mySlider.value = UserDefaults.standard.float(forKey: "Slider")
+        myTextField.text = UserDefaults.standard.string(forKey: "Text")
+    }
+    
+    
 }
 
